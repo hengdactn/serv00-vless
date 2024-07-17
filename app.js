531d4ed3c233c8129d008a2904c584383c41d1a5
飞翔 <70909689+hengdactn@users.noreply.github.com>
2024-07-17 16:42:29 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const WebSocket = require('ws');
 const logcb = (...args) => console.log.bind(this, ...args);
 const errcb = (...args) => console.error.bind(this, ...args);
 
-const uuid = (process.env.UUID || '37a0bd7c-8b9f-4693-8916-bd1e2da0a817').replace(/-/g, '');
-const port = process.env.PORT || 3000;
+const uuid = (process.env.UUID || '323f5607-f006-4de1-ad9f-979ebcad7ccf').replace(/-/g, '');
+const port = process.env.PORT || 58865;
 
 const wss = new WebSocket.Server({ port }, logcb('listen:', port));
 
